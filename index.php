ec4a71e8ff678b8e92dd9d237656ae7ff4c6406c
Zhan Setya <ardy67508@gmail.com>
2024-11-10 17:01:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+echo '';
+echo '<html>';
+echo '<head>';
+echo '<title>Hacked By Zetz Xploit</title>';
+echo '<style>';
+echo '@import url('https://fonts.googleapis.com/css2?family=Kelly+Slab&display=swap');';
+echo 'body {';
+echo 'background: #000000;';
+echo 'color: #fff;';
+echo 'text-align: center;';
+echo 'font-family: Arial, sans-serif;';
+echo 'background-image: url('');';
+echo 'background-size: cover;';
+echo 'background-position: center;';
+echo '}';
+echo 'p { font-size: 9px; margin: 0; }';
+echo 'audio { margin: 13px; }';
+echo 'marquee { margin: 13px; }';
+echo '.email { margin: 14px; font-size: 12px }';
+echo '</style>';
+echo '</head>';
+echo '<body>';
+echo '<h4>Has Been Hacked By Zetz Xploit</h4>';
+echo '<img src="https://a.top4top.io/p_3172uostb0.png" alt="" class="image" width="200" height="200">';
+echo '<marquee><h4>Your system is not safe</h4></marquee>';
+echo '<center>';
+echo '<p><b>Greets To:<br>Leviathan404 - HyperXyclic! - Mikaelz - damnboy404 - Fake404 - ./kenzXploit - ZenturaX Zenith Renx - hxcp07 - ./C10X - VN.Pisces - Jubah Hitam agency - Pron3x - Ztyumi_Exploit_Anonim - Ilham - Doom3r_ - ./AdiXploit - Cartivamp - Miko - Haket - Jateng Batang Xploit - Hackrypton - Sin Of Cyber - Cyber Warriors Team - WolfCyberArmy - Infinite Insight.ID - Semarang Cyber Error System - Jawa Timur Exploit - Sabotase Syndicate Team - All Anonymous Indonesia</p>';
+echo '</center>';
+echo '';
+echo '</body>';
+echo '</html>';
+echo '';
+?>
